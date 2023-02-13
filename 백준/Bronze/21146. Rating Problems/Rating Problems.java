6b720deb8eb8c6a76eca16dbe88b42c550d0820b
skullax9 <70187273+skullax9@users.noreply.github.com>
2023-02-13 11:13:19 +0900

    [Bronze III] Title: Rating Problems, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/21146. Rating Problems/Rating Problems.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		int n = fr.nextInt();
+		int k = fr.nextInt();
+
+		StringBuilder sb = new StringBuilder();
+
+		int[] nums = new int[n];
+
+		for (int i = 0; i < k; i++) {
+			nums[i] = fr.nextInt();
+		}
+
+		double sum = Arrays.stream(nums).sum();
+
+		double least = (sum + (-3 * (n - k))) / n;
+		double max = (sum + (3 * (n - k))) / n;
+
+		sb.append(least).append(" ").append(max);
+
+		System.out.println(sb);
+	}
+
+	public static int parse(String str) {
+		return Integer.parseInt(str);
+	}
+
+	public static String toStr(int i) {
+		return String.valueOf(i);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
