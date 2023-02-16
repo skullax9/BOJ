2589084f9f9de4c8f63401be2d2c04e75da959c2
skullax9 <70187273+skullax9@users.noreply.github.com>
2023-02-16 14:24:36 +0900

    [Bronze III] Title: 연길이의 이상형, Time: 128 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Bronze/20540. 연길이의 이상형/연길이의 이상형.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.math.*;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+import java.util.stream.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		String[] MBTI = fr.nextLine().split("");
+
+		String ESTJ = "ESTJ";
+		String INFP = "INFP";
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < 4; i++) {
+			int index = Math.max(ESTJ.indexOf(MBTI[i]), INFP.indexOf(MBTI[i]));
+
+			char a = ESTJ.charAt(index);
+			char b = INFP.charAt(index);
+
+			char c = MBTI[i].charAt(0);
+
+			sb.append(a == c ? b : a);
+		}
+
+		System.out.println(sb);
+	}
+
+	public static String convert(String str) {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == 'A') {
+				sb.append("B");
+			} else {
+				sb.append("A");
+			}
+		}
+
+		return sb.toString();
+	}
+
+	public static int parse(String str) {
+		return Integer.parseInt(str);
+	}
+
+	public static String toStr(int i) {
+		return String.valueOf(i);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
