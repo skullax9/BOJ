a3acf9f257a39deafab5210224994700fb29d5e1
skullax9 <70187273+skullax9@users.noreply.github.com>
2023-02-16 11:48:14 +0900

    [Bronze IV] Title: 치킨댄스를 추는 곰곰이를 본 임스 2, Time: 144 ms, Memory: 15140 KB -BaekjoonHub

---
+++ 백준/Bronze/26068. 치킨댄스를 추는 곰곰이를 본 임스 2/치킨댄스를 추는 곰곰이를 본 임스 2.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		int N = fr.nextInt();
+		
+		int sum = 0;
+		
+		for (int i = 0; i < N; i++) {
+			int days = parse(fr.nextLine().split("-")[1]);
+			sum += days <= 90 ? 1 : 0;
+		}
+
+		System.out.println(sum);
+		
+	}
+
+	public static String convert(String str) {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == 'A') {
+				sb.append("B");
+			} else {
+				sb.append("A");
+			}
+		}
+
+		return sb.toString();
+	}
+
+	public static int parse(String str) {
+		return Integer.parseInt(str);
+	}
+
+	public static String toStr(int i) {
+		return String.valueOf(i);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
