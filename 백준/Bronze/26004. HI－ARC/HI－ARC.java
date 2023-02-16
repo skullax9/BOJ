14c1c4e08bf9b9e9d70c30bd0baa32268b557db0
skullax9 <70187273+skullax9@users.noreply.github.com>
2023-02-16 15:25:39 +0900

    [Bronze III] Title: HI-ARC, Time: 376 ms, Memory: 24476 KB -BaekjoonHub

---
+++ 백준/Bronze/26004. HI－ARC/HI－ARC.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		int N = fr.nextInt();
+		List<String> list = new ArrayList<>(List.of(fr.nextLine().split("")));
+		
+		long min = list.stream().filter(f -> f.equals("H")).count();
+		min = Math.min(min, list.stream().filter(f -> f.equals("I")).count());
+		min = Math.min(min, list.stream().filter(f -> f.equals("A")).count());
+		min = Math.min(min, list.stream().filter(f -> f.equals("R")).count());
+		min = Math.min(min, list.stream().filter(f -> f.equals("C")).count());
+
+		System.out.println(min);
+ 	}
+
+	public static String convert(String str) {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == 'A') {
+				sb.append("B");
+			} else {
+				sb.append("A");
+			}
+		}
+
+		return sb.toString();
+	}
+
+	public static int parse(String str) {
+		return Integer.parseInt(str);
+	}
+
+	public static String toStr(int i) {
+		return String.valueOf(i);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
