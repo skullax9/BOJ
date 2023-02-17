83c6b42813d140168ce0a2f29f16c62403075ca6
skullax9 <70187273+skullax9@users.noreply.github.com>
2023-02-17 13:34:13 +0900

    [Bronze II] Title: 도비의 영어 공부, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/2386. 도비의 영어 공부/도비의 영어 공부.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		StringBuilder sb = new StringBuilder();
+
+		while (true) {
+			String str = fr.nextLine();
+
+			if (str.equals("#")) break;
+
+			final String src = str.substring(0,1);
+
+			String[] sp = String.join("", str.split(" ")).substring(1).toLowerCase().split("");
+
+			List<String> list = new ArrayList<>(List.of(sp));
+
+			long cnt = list.stream().filter(f -> f.equals(src)).count();
+
+			sb.append(src).append(" ").append(cnt).append("\n");
+		}
+
+		System.out.println(sb);
+
+	}
+
+	public static int parse(String str) {
+		return Integer.parseInt(str);
+	}
+
+	public static String toStr(int i) {
+		return String.valueOf(i);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
