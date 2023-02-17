66a8fb82affd7afd4669c3263e4c2f3a595793fd
skullax9 <70187273+skullax9@users.noreply.github.com>
2023-02-17 09:02:41 +0900

    [Bronze II] Title: 더하기, Time: 128 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Bronze/10822. 더하기/더하기.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		System.out.println(Arrays.stream(fr.nextLine().split(",")).mapToInt(Integer::parseInt).sum());
+ 	}
+
+	public static int parse(String str) {
+		return Integer.parseInt(str);
+	}
+
+	public static String toStr(int i) {
+		return String.valueOf(i);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
