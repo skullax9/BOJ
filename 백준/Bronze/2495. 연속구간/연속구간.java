2847b948c74a408ffceda7ab4b6fa9e74713272e
skullax9 <70187273+skullax9@users.noreply.github.com>
2023-02-17 09:38:10 +0900

    [Bronze II] Title: 연속구간, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/2495. 연속구간/연속구간.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < 3; i++) {
+			String str = fr.nextLine() + "A";
+
+			int max = 1;
+			int inc = 1;
+
+			for (int j = 1; j < str.length(); j++) {
+				if (str.charAt(j - 1) == str.charAt(j)) {
+					inc++;
+				} else {
+					max = Math.max(max, inc);
+					inc = 1;
+				}
+			}
+
+			sb.append(max).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+	public static int parse(String str) {
+		return Integer.parseInt(str);
+	}
+
+	public static String toStr(int i) {
+		return String.valueOf(i);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
