3ba9bab105414ee38950d30d5bd413dd61c2cdd2
skullax9 <70187273+skullax9@users.noreply.github.com>
2023-02-17 13:50:06 +0900

    [Bronze II] Title: 해밍 거리, Time: 128 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/3449. 해밍 거리/해밍 거리.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		int T = fr.nextInt();
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < T; i++) {
+			String[] s1 = fr.nextLine().split("");
+			String[] s2 = fr.nextLine().split("");
+
+			int sum = 0;
+
+			sb.append("Hamming distance is ");
+
+			for (int j = 0; j < s1.length; j++) {
+				if (!s1[j].equals(s2[j])) sum++;
+			}
+			sb.append(sum).append(".").append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+	public static int parse(String str) {
+		return Integer.parseInt(str);
+	}
+
+	public static String toStr(int i) {
+		return String.valueOf(i);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
