e05c8104db46713672637fb8402d607c60afb146
skullax9 <70187273+skullax9@users.noreply.github.com>
2023-02-20 09:46:42 +0900

    [Silver V] Title: 복붙의 달인, Time: 232 ms, Memory: 18356 KB -BaekjoonHub

---
+++ 백준/Silver/11008. 복붙의 달인/복붙의 달인.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		StringBuilder sb = new StringBuilder();
+
+		int T = fr.nextInt();
+
+		for (int i = 0; i < T; i++) {
+			String[] sp = fr.nextLine().split(" ");
+
+			String word = sp[0].replaceAll(sp[1],"1");
+
+			sb.append(word.length()).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+	public static int parse(String str) {
+		return Integer.parseInt(str);
+	}
+
+	public static String toStr(int i) {
+		return String.valueOf(i);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
