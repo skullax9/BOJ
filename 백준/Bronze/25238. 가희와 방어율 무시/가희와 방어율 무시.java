c044f39da7775a4bde16e7dd43130d259f08f96c
skullax9 <70187273+skullax9@users.noreply.github.com>
2023-02-22 09:42:27 +0900

    [Bronze V] Title: 가희와 방어율 무시, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/25238. 가희와 방어율 무시/가희와 방어율 무시.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		StringBuilder sb = new StringBuilder();
+
+		int a = fr.nextInt();
+		int b = fr.nextInt();
+
+		double calc = (double) a * (100 - b) / 100;
+
+		System.out.println(calc < 100 ? 1 : 0);
+	}
+
+	static int euclidean(int i, int k) {
+		int r = Math.max(i, k) % Math.min(i, k);
+
+		if (r == 0) {
+			return k;
+		} else {
+			return euclidean(k, r);
+		}
+	}
+
+	public static int parse(String str) {
+		return Integer.parseInt(str);
+	}
+
+	public static String toStr(int i) {
+		return String.valueOf(i);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
