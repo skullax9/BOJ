76817105f2ad6db183d4666bd020fc4e9f6acbf4
skullax9 <70187273+skullax9@users.noreply.github.com>
2023-04-21 15:14:59 +0900

    [unrated] Title: 문자열 돌리기, Time: 167.99 ms, Memory: 65.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        FastReader fr = new FastReader();
+        
+        StringBuilder sb = new StringBuilder();
+        
+        String str = fr.next();
+        
+        for (char c : str.toCharArray()) {
+            sb.append(c).append("\n");
+        }
+        
+        System.out.println(sb.toString());
+    }
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
