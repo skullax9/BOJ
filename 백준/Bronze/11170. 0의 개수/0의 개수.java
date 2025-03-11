a25e839f9490914b07effd71f4d8a2a5eb32f2c6
skullax9 <70187273+skullax9@users.noreply.github.com>
2025-03-11 15:12:06 +0900

    [Bronze I] Title: 0의 개수, Time: 2444 ms, Memory: 311340 KB -BaekjoonHub

---
+++ 백준/Bronze/11170. 0의 개수/0의 개수.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		int T = fr.nextInt();
+
+		for (int i = 0; i < T; i++) {
+			int N = fr.nextInt();
+			int M = fr.nextInt();
+
+			String st = IntStream.rangeClosed(N, M).boxed().map(String::valueOf).collect(Collectors.joining());
+
+			st = st.replaceAll("[1-9]", "");
+
+			System.out.println(st.length());
+		}
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
