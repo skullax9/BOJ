b6be4413831006586df361189c4374a5b72aba05
skullax9 <70187273+skullax9@users.noreply.github.com>
2025-03-11 14:53:17 +0900

    [Bronze II] Title: 대회 자리, Time: 132 ms, Memory: 14844 KB -BaekjoonHub

---
+++ 백준/Bronze/5176. 대회 자리/대회 자리.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		int K = fr.nextInt();
+
+		for (int i = 0; i < K; i++) {
+			int P = fr.nextInt();
+			int M = fr.nextInt();
+
+			Set<Integer> list = IntStream.rangeClosed(1, M).boxed().collect(Collectors.toSet());
+
+			int success = 0;
+
+			for (int j = 0; j < P; j++) {
+				int seat = fr.nextInt();
+
+				if (list.remove(seat)) success++;
+			}
+
+			System.out.println(P - success);
+		}
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
