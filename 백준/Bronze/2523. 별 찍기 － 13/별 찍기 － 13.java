c491181e9adf5a2f06911d2d050c138cdc0a89ab
skullax9 <70187273+skullax9@users.noreply.github.com>
2025-03-12 10:28:19 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 108 ms, Memory: 14428 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		int N = fr.nextInt();
+
+		StringBuilder sb =  new StringBuilder();
+		List<String> list = new ArrayList<>();
+
+		for (int i = 1; i <= N; i++) {
+			if (i == N) {
+				sb.append("*".repeat(i)).append("\n");
+				for (int j = list.size() - 1; j >= 0; j--) {
+					sb.append(list.get(j));
+				}
+			} else {
+				list.add("*".repeat(i) + "\n");
+				sb.append("*".repeat(i)).append("\n");
+			}
+		}
+
+		System.out.println(sb);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
