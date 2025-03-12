04e2c5bfef26f598f5dddade3e70c15ed04e417a
skullax9 <70187273+skullax9@users.noreply.github.com>
2025-03-12 09:34:41 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		String answer = "You get %s piece(s) and your dad gets %s piece(s).";
+
+		int T = fr.nextInt();
+
+		for (int i = 0; i < T; i++) {
+			int c = fr.nextInt();
+			int v = fr.nextInt();
+
+			System.out.println(String.format(answer, c / v, c % v));
+		}
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
