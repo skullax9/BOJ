cee0e59771b5b433589ab4747f28472009d99543
skullax9 <70187273+skullax9@users.noreply.github.com>
2025-03-14 15:48:40 +0900

    [Bronze III] Title: Sum of Odd Sequence, Time: 112 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/9713. Sum of Odd Sequence/Sum of Odd Sequence.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		StringBuilder sb = new StringBuilder();
+
+		int T = fr.nextInt();
+
+		for (int i = 0; i < T; i++) {
+			int odd = fr.nextInt();
+
+			sb.append(IntStream.rangeClosed(1, odd).filter(f -> f % 2 == 1).sum()).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
