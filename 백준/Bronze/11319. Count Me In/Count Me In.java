7272890373bdf85c34816087cca6eb8d8cae136a
skullax9 <70187273+skullax9@users.noreply.github.com>
2025-03-14 16:05:00 +0900

    [Bronze II] Title: Count Me In, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/11319. Count Me In/Count Me In.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		StringBuilder sb = new StringBuilder();
+
+		int S = fr.nextInt();
+
+		for (int i = 0; i < S; i++) {
+			String sentence = fr.nextLine().replaceAll(" ", "");
+
+			int len = sentence.length();
+
+			sentence = sentence.replaceAll("[aeiouAEIOU]", "");
+
+			sb.append(sentence.length()).append(" ").append(len - sentence.length()).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
