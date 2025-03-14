32cafe9de690ed873e71fabc79897eed4fd0a7de
skullax9 <70187273+skullax9@users.noreply.github.com>
2025-03-14 16:35:20 +0900

    [Bronze III] Title: Buffoon, Time: 132 ms, Memory: 15432 KB -BaekjoonHub

---
+++ 백준/Bronze/17530. Buffoon/Buffoon.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		StringBuilder sb = new StringBuilder();
+
+		int N = fr.nextInt();
+
+		int c = fr.nextInt();
+		int max = -1;
+
+		for (int i = 1; i < N; i++) {
+			max = Math.max(max, fr.nextInt());
+		}
+
+		System.out.println( c >= max ? "S" : "N");
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
