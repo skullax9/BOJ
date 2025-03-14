43659a36534a4139d94360a53812d25fda0bff37
skullax9 <70187273+skullax9@users.noreply.github.com>
2025-03-14 16:01:39 +0900

    [Bronze II] Title: Fridge of Your Dreams, Time: 112 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/11104. Fridge of Your Dreams/Fridge of Your Dreams.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		StringBuilder sb = new StringBuilder();
+
+		int n = fr.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			String tf = fr.nextLine();
+
+			long sum = 0;
+
+			for (int j = 0; j < tf.length(); j++) {
+				if (tf.charAt(j) == '1') {
+					sum += (long) Math.pow(2, tf.length() - j - 1);
+				}
+			}
+
+			sb.append(sum).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
