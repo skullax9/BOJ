ea8aa9af7fc6f8ce4a345d477d0238877f0d1a9e
skullax9 <70187273+skullax9@users.noreply.github.com>
2025-03-17 14:31:03 +0900

    [Bronze II] Title: 탄산 음료, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/5032. 탄산 음료/탄산 음료.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		StringBuilder sb = new StringBuilder();
+
+		int e = fr.nextInt();
+		e += fr.nextInt();
+		int c = fr.nextInt();
+
+		int cnt = 0;
+
+		while (e > 0) {
+			e -= c;
+			e++;
+			if (e > 0) cnt++;
+		}
+
+		System.out.println(cnt);
+	}
+
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
