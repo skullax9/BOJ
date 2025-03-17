c72f16b410ff453c066e60037a39897d62176193
skullax9 <70187273+skullax9@users.noreply.github.com>
2025-03-17 13:42:32 +0900

    [Bronze III] Title: 체스판 조각, Time: 96 ms, Memory: 14024 KB -BaekjoonHub

---
+++ 백준/Bronze/3004. 체스판 조각/체스판 조각.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		int N = fr.nextInt();
+		
+		int sum = 2;
+
+		for(int i = 2; i <= N; i++) {
+			sum += ((i / 2) + 1);
+		}
+
+		System.out.println(sum);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
