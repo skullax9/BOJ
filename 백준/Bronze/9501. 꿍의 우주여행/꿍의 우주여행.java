18c0f889f17a9b38a046b504ebba2a31e326a86a
skullax9 <70187273+skullax9@users.noreply.github.com>
2025-03-17 16:50:53 +0900

    [Bronze III] Title: 꿍의 우주여행, Time: 132 ms, Memory: 15400 KB -BaekjoonHub

---
+++ 백준/Bronze/9501. 꿍의 우주여행/꿍의 우주여행.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		StringBuilder sb = new StringBuilder();
+
+		int T = fr.nextInt();
+
+		for (int i = 0; i < T; i++) {
+			int N = fr.nextInt();
+			long D = fr.nextLong();
+
+			int sum = 0;
+
+			for (int j = 0; j < N; j++) {
+				// 최고속도
+				int v = fr.nextInt();
+				// 연료의 양
+				int f = fr.nextInt();
+				// 연료 소비율
+				int c = fr.nextInt();
+
+				double yeonbi = (double) v / (double) c;
+
+				if (D <= yeonbi * f) sum++;
+			}
+
+			sb.append(sum).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
