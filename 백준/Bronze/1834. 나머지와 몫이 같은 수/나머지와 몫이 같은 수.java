5ec3b8b387186f06d17a1c7a35195bd9fd15e760
skullax9 <70187273+skullax9@users.noreply.github.com>
2025-03-17 10:48:10 +0900

    [Bronze I] Title: 나머지와 몫이 같은 수, Time: 796 ms, Memory: 301328 KB -BaekjoonHub

---
+++ 백준/Bronze/1834. 나머지와 몫이 같은 수/나머지와 몫이 같은 수.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		StringBuilder sb = new StringBuilder();
+
+		int N = fr.nextInt();
+
+		BigInteger bigInteger = BigInteger.ZERO;
+
+		for (long i = N; i < (long) N * N; i++) {
+			if (i / N == i % N) {
+				bigInteger = bigInteger.add(new BigInteger(String.valueOf(i)));
+				i += N;
+			}
+		}
+
+		System.out.println(bigInteger);
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
