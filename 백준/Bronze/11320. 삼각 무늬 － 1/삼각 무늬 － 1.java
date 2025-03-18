69b0d855aad79de003756e53a8f071165a2f28d1
skullax9 <70187273+skullax9@users.noreply.github.com>
2025-03-18 10:01:04 +0900

    [Bronze III] Title: 삼각 무늬 - 1, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/11320. 삼각 무늬 － 1/삼각 무늬 － 1.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		StringBuilder sb = new StringBuilder();
+
+		int T = fr.nextInt();
+
+		for (int i = 0; i < T; i++) {
+			int A = fr.nextInt();
+			int B = fr.nextInt();
+
+			Double div = Math.pow(A, 2) / Math.pow(B, 2);
+
+			int need = div.intValue() + (div - div.intValue() != 0 ? 1 : 0);
+
+			sb.append(need).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
