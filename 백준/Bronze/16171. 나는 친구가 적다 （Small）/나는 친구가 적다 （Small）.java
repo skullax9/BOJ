7b445dcc1b8d479e67cab3d6859e2df07adea7fa
skullax9 <70187273+skullax9@users.noreply.github.com>
2025-03-19 09:42:09 +0900

    [Bronze II] Title: 나는 친구가 적다 (Small), Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/16171. 나는 친구가 적다 （Small）/나는 친구가 적다 （Small）.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+
+		StringBuilder sb = new StringBuilder();
+
+		String src = fr.nextLine();
+		String target = fr.nextLine();
+		
+		src = src.replaceAll("[0-9]", "");
+
+		System.out.println(src.contains(target) ? 1 : 0);
+	}
+
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
